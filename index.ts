c187d8155e0833b3f695c70281a598fb627b2fdc
MasudRiam <masudp.riam@gmail.com>
2025-05-19 21:40:19 +0600

    learn day-3

--- index.ts
+++ index.ts
@@ -16,7 +16,6 @@ console.log (inputUserData)
 
 
 
-
 class User2 {
     constructor (public name: string, public age: number, private email: string, private password: number, private isActive: boolean) {
     }
@@ -27,6 +26,35 @@ const inputUser2: User = new User (inputUserData.name, inputUserData.age, inputU
 console.log (inputUser2)
 
 
+
+
+
+//getter and setter
+class Admin {
+    private _cours = 1;
+
+
+    constructor (
+        public name: string,
+        public age: number) {
+    }
+
+    get adminName (): string {
+        return this.name;
+    }
+    get adminAge (): number {
+        return this.age;
+    }
+    set courseCount (courseNumber) {
+        if (courseNumber <= 1) {
+            throw new Error ("The course should be more then 1")
+        }
+        this._cours = courseNumber;
+    }
+}
+
+const adminUser = new Admin ("Riam", 25)
+console.log (adminUser)
 
 
 
